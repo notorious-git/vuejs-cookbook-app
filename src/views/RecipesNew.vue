43c1be073bf0a3c89a25398ec1890eef830d4e95
Peter Jang <peter.x.jang@gmail.com>
2018-11-19 20:09:48 -0600

    Display errors, make new recipes separate page

--- src/views/RecipesNew.vue
+++ src/views/RecipesNew.vue
@@ -1,13 +1,6 @@
 <template>
   <div class="home">
     <div class="container">
-      <div class="jumbotron">
-        <h1 class="display-4">Hello, world!</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-      </div>
 
       <h1>Add new recipe</h1>
       <ul>
@@ -19,19 +12,6 @@
       Directions: <input v-model="newRecipeDirections" type="text">
       <button v-on:click="createRecipe()" class="btn btn-primary">Create</button>
 
-      <div class="row">
-        <div v-for="recipe in recipes" class="col-md-4 mb-2">
-          <div class="card">
-            <img class="card-img-top" v-bind:src="recipe.image_url" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">{{ recipe.title }}</h5>
-              <p class="card-text">Chef: {{ recipe.chef }}</p>
-              <p class="card-text">Ingredients: {{ recipe.ingredients }}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -45,8 +25,6 @@ var axios = require("axios");
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!",
-      recipes: [],
       newRecipeName: "",
       newRecipeChef: "",
       newRecipeIngredients: "",
@@ -54,14 +32,7 @@ export default {
       errors: []
     };
   },
-  created: function() {
-    axios.get("http://localhost:3000/api/recipes").then(
-      function(response) {
-        console.log(response.data);
-        this.recipes = response.data;
-      }.bind(this)
-    );
-  },
+  created: function() {},
   methods: {
     createRecipe: function() {
       console.log("createRecipe");
@@ -77,11 +48,11 @@ export default {
         .then(
           function(response) {
             console.log(response);
-            this.recipes.push(response.data);
             this.newRecipeName = "";
             this.newRecipeChef = "";
             this.newRecipeIngredients = "";
             this.newRecipeDirections = "";
+            this.$router.push("/");
           }.bind(this)
         )
         .catch(
