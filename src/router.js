8d2d1c7169d5942f8ae02040194ea7104bd89cfd
Peter Jang <peter.x.jang@gmail.com>
2018-11-20 18:34:03 -0600

    Make a recipes show component with hard coded web request

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import RecipesNew from "./views/RecipesNew.vue";
+import RecipesShow from "./views/RecipesShow.vue";
 import Signup from "./views/Signup.vue";
 import Login from "./views/Login.vue";
 import Logout from "./views/Logout.vue";
@@ -23,6 +24,7 @@ export default new Router({
     { path: "/signup", name: "signup", component: Signup },
     { path: "/login", name: "login", component: Login },
     { path: "/logout", name: "logout", component: Logout },
+    { path: "/recipes/:id", name: "recipes-show", component: RecipesShow },
     {
       path: "/about",
       name: "about",
---
+++ src/views/RecipesShow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <h1>Recipe info</h1>
+      {{ recipe }}
+      <a href="/#/">Back to all recipes</a>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+var axios = require("axios");
+
+export default {
+  data: function() {
+    return {
+      recipe: {}
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/recipes/3").then(response => {
+      this.recipe = response.data;
+    });
+  },
+  methods: {},
+  computed: {}
+};
+</script>
