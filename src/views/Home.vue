cede3b1fdb0bcac2094337d4b18e4cd9a0664302
Peter Jang <peter.x.jang@gmail.com>
2018-11-20 19:09:52 -0600

    Use modal on recipes index page to show more info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,10 +28,36 @@
               <p class="card-text">Chef: {{ recipe.chef }}</p>
               <p class="card-text">Ingredients: {{ recipe.ingredients }}</p>
               <a v-bind:href="`/#/recipes/${recipe.id}`" class="btn btn-primary">Go somewhere</a>
+
+              <button v-on:click="setCurrentRecipe(recipe)" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                Launch demo modal
+              </button>
+
             </div>
           </div>
         </div>
       </div>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">{{ currentRecipe.title }}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              {{ currentRecipe.ingredients }}
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -47,6 +73,7 @@ export default {
     return {
       message: "Welcome to Vue.js!",
       recipes: [],
+      currentRecipe: {},
       newRecipeName: "",
       newRecipeChef: "",
       newRecipeIngredients: "",
@@ -63,6 +90,9 @@ export default {
     );
   },
   methods: {
+    setCurrentRecipe: function(inputRecipe) {
+      this.currentRecipe = inputRecipe;
+    },
     createRecipe: function() {
       console.log("createRecipe");
       this.errors = [];
